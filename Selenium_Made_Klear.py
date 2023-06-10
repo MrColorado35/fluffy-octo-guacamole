db34e11f1319780d2813c1618f17eb6af2e142e7
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2023-06-10 19:31:59 +0000

    interesting error with the driver, need further investigation (add click_btn function)

--- Selenium_Made_Klear.py
+++ Selenium_Made_Klear.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from seleniumwire import webdriver
 from random import randrange
 from selenium import webdriver
-from selenium_proxy import SeleniumProxy 
+# from selenium_proxy import SeleniumProxy 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -21,14 +21,15 @@ class Selenium_Clear:
     def __init_(self):
         self.client = MongoClient("mongodb://localhost:27017/")
         self.params = ({'in_progress': 0, 'failed': 0, 'complete': 0})
+        self.driver = self.start_driver()
 
      # Create and start the driver  
-    def start_driver(self, proxy=""):
-        if proxy != "":
-            SP = SeleniumProxy()
-            self.driver = SP.get_ip_via_chrome()
-        else:
-            self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    def start_driver(self, proxy=False):
+        # if proxy == True:
+        #     SP = SeleniumProxy()
+        #     self.driver = SP.get_ip_via_chrome()
+        # else:
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         return self.driver
 
     """Call the driver and open required page. In case if we won't provide any url, it will go to the deafault one"""
@@ -36,10 +37,31 @@ class Selenium_Clear:
         # Variable "website" should be specified every time before running it, but in case if it was left blank we need a page that will do no harm to our program
         if website == "":
             website = "https://www.sportinglife.com/racing/fast-results"
+        else:
+            website == website
         self.main_url = website
         self.driver.get(self.main_url)
         # Depending on the URL, some delay is required for program to wait until the website is fully loaded. That part will be replaced with WebDriverWait when I can spent some time poilishing this program
         sleep(4)
 
+    """This function will take care of clicking any button.
+    At the moment it only can search by ID or CSS selector, extend 'except' in case if you need Xpath/ Name etc.
+    Just pass the string with search criteria for the driver and call it in your code
+    """
+    def click_btn(self, btn):
+        try:
+            click_me = self.driver.find_element(By.ID, btn)
+            print("GOT THE ID")
+        except:
+            click_me = self.driver.find_element(By.CSS_SELECTOR, btn)
+            print("GOT THE CSS SELECTOR")
+            
+        click_me.click()
+        sleep(3)
 
 
+if __name__ == '__main__':
+    app = Selenium_Clear()
+    # driver = app.start_driver()
+    website = "https://www.sportinglife.com/racing/fast-results#"
+    app.open_page(website)
